f6d8bf682003f3ce9e6bc4f529209c566ce28262
ilopjim1306 <ilopjim1306@g.educaand.es>
2023-10-09 21:38:42 +0200

    09/10/2023

--- EjerciciosU1/ejercicio22.py
+++ EjerciciosU1/ejercicio22.py
@@ -1,4 +1,6 @@
 frase = input("Introduce una frase: ")
 vocal = input("Introduce una vocal: ")
 
-print(f"{vocal.upper()} {frase}.")
+frase = frase.replace(vocal.lower,vocal.upper)
+
+print(f"{frase}.")
