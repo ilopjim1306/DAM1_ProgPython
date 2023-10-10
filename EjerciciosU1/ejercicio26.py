56186e10bca62aef17a172ba2fa0220423758013
ilopjim1306 <ilopjim1306@g.educaand.es>
2023-10-10 12:27:40 +0200

    Prueba casa

--- EjerciciosU1/ejercicio26.py
+++ EjerciciosU1/ejercicio26.py
@@ -1,4 +1,4 @@
-productos = input("Introduce los productos de la cesta de la compra separado por comas: ")
+productos = input("Introduce los productos de la cesta de la compra separado por comas sin espacios: ")
 productos = productos.split(",")
 
 print ('\n' .join(map(str, productos)))
