9ab0f1cfec4ab816dcce52f64356d09c43dd2969
ilopjim1306 <145800013+ilopjim1306@users.noreply.github.com>
2023-10-10 11:01:54 +0200

    Update ejercicio22.py

--- EjerciciosU1/ejercicio22.py
+++ EjerciciosU1/ejercicio22.py
@@ -1,6 +1,6 @@
 frase = input("Introduce una frase: ")
 vocal = input("Introduce una vocal: ")
 
-frase = frase.replace(vocal.lower,vocal.upper)
+frase = frase.replace(vocal.lower(),vocal.upper())
 
 print(f"{frase}.")
