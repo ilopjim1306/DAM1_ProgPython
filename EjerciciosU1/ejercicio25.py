b88adc269f630f3c3449a6565d1de9c0f6951c6b
ilopjim1306 <145800013+ilopjim1306@users.noreply.github.com>
2023-10-10 12:09:41 +0200

    Update ejercicio25.py

--- EjerciciosU1/ejercicio25.py
+++ EjerciciosU1/ejercicio25.py
@@ -2,6 +2,6 @@ fecha = input("Introduce la fecha en formato dd/mm/aaaa: ")
 fecha1 = fecha.split("/")
 dia= int(fecha1[0])
 mes = int(fecha1[1])
-anio= fecha1[2]
+anio= int(fecha1[2])
 
 print("Día {:02d} del mes {:02d} del año {}" .format(dia, mes, anio))
