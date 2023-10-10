9b51498b4331dfe898db7e429e5b29e9ef3e403a
ilopjim1306 <ilopjim1306@g.educaand.es>
2023-10-10 14:10:27 +0200

    ejercicios

--- EjerciciosU1/ejercicio27.py
+++ EjerciciosU1/ejercicio27.py
@@ -5,5 +5,4 @@ precio = float(producto[1])
 cantidad = int(producto[2])
 total = precio * cantidad
 
-print(f"{nombre.title()}, su precio es de {round(precio,2)}€, con una cantidad de {round(cantidad,2)} unidades y el coste total es de: {round(total, 2)}€")
-print("El nombre del producto es: " , nombre.title() , ", su precio es de: {:6.2f}€, se han vendido un total de {:03d} unidades y el coste total es de: {:8.2f}€.".format(precio, cantidad, total))
+print("El nombre del producto es: {}, su precio es de: {:6.2f}€, se han vendido un total de {:3d} unidades y el coste total es de: {:8.2f}€.".format(nombre, precio, cantidad, total))
