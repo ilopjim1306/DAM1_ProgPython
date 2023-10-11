031051f94c30096b18865a59f7a13a6218c6f3c5
ilopjim1306 <ilopjim1306@g.educaand.es>
2023-10-11 18:49:52 +0200

    ejercicio30

--- EjerciciosU1/ejercicio30.py
+++ EjerciciosU1/ejercicio30.py
@@ -1,31 +1,51 @@
 #Inicio
+#   Escribe "Dame un número de inicio: "
 #   Lee inicio
+#   Escribe "Dame un número de incremento: "
 #   Lee incremento
+#   Escribe "Dame un número final: "
 #   Lee total
-#   Mientras (incremento < 0 and total < 0)
-#       Escribe "Error introduce un incremento mayor a 0: "
-#       Lee incremento
-#   Sino entonces
-#       Mientras(total => 0)
+#   serie = "SERIE => " + inicio
+#   cont = 1
+#   Si (incremento < 0) entonces
+#       Mientras (incremento < 0)
+#           Escribe "Error introduce un incremento mayor a 0: "
+#           Lee incremento
+#   Si (total < 0) entonces
+#       Mientras(total < 0)
 #           Escribe "Error introduce un total mayor a 0: "
 #           Lee total
-#   Para i en (inicio...total) hacer
-#       suma = inicio + incremento 
-#       Escribe suma       
-#
+#   mientras (cont < total) hacer
+#       inicio = inicio + incremento       
+#       cont = cont + 1
+#       Si (cont < (total - 1)) entonces
+#           serie = serie + inicio + ".."
+#       Sino 
+#           Si (cont == total) entonces
+#               serie = serie + inicio
+#           Sino 
+#               serie = seroe + inicio + "-"
 #Fin
 
 inicio = int(input("Dame un número de inicio: "))
 incremento = int(input("Dame un número de incremento: "))
 total = int(input("Dame un número final: "))
+serie = str("SERIE => ") + str(inicio) +  str("-")
+cont = 1
 
-if(incremento <= 0):
-    while(incremento <= 0):
-        incremento = int(input("Error introduce un incremento mayor a 0: "))
-if(total <= 0):
-    while(total <= 0):
-        total = int(input("Error introduce un final mayor a 0: "))
-while (inicio < total):
+while(incremento <= 0 or total <= 0):
+    incremento = int(input("Error introduce un incremento mayor a 0: "))
+    total = int(input("Error introduce un final mayor a 0: "))
+
+while (cont < total):
     inicio = inicio + incremento
-    print(inicio)
+    cont = cont + 1
+    if (cont < (total - 1)):
+        serie = str(serie) + str(inicio) + str("..")
+    else:
+        if (cont == total):
+            serie = str(serie) + str(inicio)
+        else:
+            serie = str(serie) + str(inicio) + str("-")
 
+print(serie)
