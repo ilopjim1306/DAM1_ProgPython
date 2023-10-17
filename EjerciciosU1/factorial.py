90012f9382c21a283b575f079ba505aabf00e79c
ilopjim1306 <ilopjim1306@g.educaand.es>
2023-10-17 19:52:13 +0200

    factorial empezado en clase

---
+++ EjerciciosU1/factorial.py
@@ -0,0 +1,13 @@
+def factorial(n):
+    if n < 0:
+        print("Error")
+    else:
+        while n >0:
+            n = n * (n + 1)
+        
+
+
+n = int(input("Introduce un número de inicio: "))
+
+cont=0
+cont *= 1
