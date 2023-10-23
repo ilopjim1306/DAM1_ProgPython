447ec201afd55304cab42f6c5d01e86ee7cfc93e
ilopjim1306 <ilopjim1306@g.educaand.es>
2023-10-23 21:36:10 +0200

    p1.10 empezado desde clase

--- pruebas/prueba1.py
+++ pruebas/prueba1.py
@@ -1,12 +1,14 @@
-def suma(num1, num2):
-    return num1 + num2
+#Desarrolla una función en prueba1.py que reciba dos números y retorne el mayor número de los dos o 0 si son iguales. Realiza las pruebas unitarias y ejecútalas con pytest desde la terminal (puedes hacerlo en la terminal dentro de Visual Studio Code).
 
-print(suma(3, 2))
-print("La suma de", 3, "+", 2, "es", suma(3, 2))
-print("La suma de " + str(3) + " + " + str(2) + " es " + str(suma(3, 2)))
-res = "La suma de "
-res += str(3)
-res += " + " + str(2)
-res += " es " + str(suma(3, 2))
-print(res)
-print(f"La suma de {3} + {2} es {suma(3, 2)}")
+def mayor(num1, num2):
+    if num1 > num2:
+        return num1
+    elif num1 == num2:
+        return 0
+    else:
+        return num2
+    
+num1 = 6
+num2 = 3
+
+print(mayor(num1, num2))
